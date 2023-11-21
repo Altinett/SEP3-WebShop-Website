205fa683b8e2ec2bef9a30e1f851cfdfafca99ab
AHowson <Alexander.Howson@gmail.com>
2023-11-21 12:55:54 +0100

    Removed some redundant atributes in the Procuct clas and fixed it where nesesary.

--- Shared/Product.cs
+++ Shared/Product.cs
@@ -5,11 +5,11 @@ namespace Shared;
 public class Product
 {
 	public int Id { get; set; }
-	public string Img { get; set; }
-	public string Title { get; set; }
+	public string Image { get; set; }
+	//public string Title { get; set; }
 	public string? name { get; set; }
 	public double Price { get; set; }
-	public double price { get; set; }
+	//public double price { get; set; }
 
 	public int? amount { get; set; }
 	public List<int>? categoryIds { get; set; }
@@ -19,14 +19,14 @@ public class Product
 
 	public Product()
 	{
-		Img = "https://i.ebayimg.com/images/g/h9UAAOSwpPNlLMxt/s-l500.jpg";
+		Image = "https://i.ebayimg.com/images/g/h9UAAOSwpPNlLMxt/s-l500.jpg";
 		//TODO Make it so that it gets a image from the database instead of this default.
 	}
 	public Product(int id, string img, string title, double price, bool inStock)
 	{
 		Id = id;
-		Img = img;
-		Title = title;
+		Image = img;
+		name = title;
 		Price = price;
 		InStock = inStock;
 	}
@@ -35,8 +35,8 @@ public class Product
 	public Product(int id, string img, string title, double price, bool inStock, int? amount, String description)
 	{
 		Id = id;
-		Img = img;
-		Title = title;
+		Image = img;
+		name = title;
 		Price = price;
 		InStock = inStock;
 		this.amount = amount;
@@ -47,8 +47,8 @@ public class Product
 	public Product(int id, string name, double price, int amount, List<int> categoryIds, string description)
 	{
 		this.Id = id;
-		Img = "https://i.ebayimg.com/images/g/h9UAAOSwpPNlLMxt/s-l500.jpg";
-		this.Title = name;
+		Image = "https://i.ebayimg.com/images/g/h9UAAOSwpPNlLMxt/s-l500.jpg";
+		this.name = name;
 		this.Price = price;
 
 		if (amount > 0)
@@ -63,19 +63,19 @@ public class Product
 		// Assuming these are the properties of Product
 		this.Id = productToCopy.Id;
 		this.amount = productToCopy.amount;
-		this.Title = productToCopy.name;
-		this.Price = productToCopy.price;
+		this.name = productToCopy.name;
+		this.Price = productToCopy.Price;
 		// ...copy all the other properties from productToCopy...
 	}
 	public override string ToString()
 	{
 		var categoryIdsString = categoryIds != null ? string.Join(", ", categoryIds) : "None";
 		return $"Product ID: {Id}\n" +
-		       $"Image URL: {Img}\n" +
-		       $"Title: {Title}\n" +
+		       $"Image URL: {Image}\n" +
+		       //$"Title: {Title}\n" +
 		       $"Name: {name}\n" +
 		       $"Price: {Price}\n" +
-		       $"Duplicate Price (might be an error): {price}\n" + // This line might need to be removed if 'price' is indeed a duplicate
+		       //$"Duplicate Price (might be an error): {price}\n" + // This line might need to be removed if 'price' is indeed a duplicate
 		       $"Amount: {amount?.ToString() ?? "Not specified"}\n" +
 		       $"Categories: {categoryIdsString}\n" +
 		       $"Description: {description}\n" +
