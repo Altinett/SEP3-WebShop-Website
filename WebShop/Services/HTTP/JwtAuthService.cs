5e4ae13743e6d86ea9d0b93cb92a339b475db3fd
JonasPHenriksen <jonashenriksenprivat@gmail.com>
2023-11-23 14:56:45 +0100

    Admin info, added to admin page, fixed time for order details

--- WebShop/Services/HTTP/JwtAuthService.cs
+++ WebShop/Services/HTTP/JwtAuthService.cs
@@ -15,10 +15,12 @@ public class JwtAuthService : IAuthService
     private readonly HttpClient client = new ();
 
     // this private variable for simple caching
-    public static string? Jwt { get; private set; } = "";
+    private static string? Jwt { get;  set; } = "";
 
     public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; } = null!;
-
+    
+    private UserDto UserDto { get; set; }
+    
     public async Task LoginAsync(string username, string password)
     {
         /*
@@ -39,19 +41,24 @@ public class JwtAuthService : IAuthService
        */
 
 
-        UserDto? userDto =  await GetFromLogic(username, password);
+        UserDto =  await GetFromLogic(username, password);
         
-        if (userDto == null)
+        if (UserDto == null)
         {
             throw new Exception("Wrong user");
         }
         
-        string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKV1RTZXJ2aWNlQWNjZXNzVG9rZW4iLCJqdGkiOiIwMzg4ZGNmMC1kYTNlLTQwNGMtYWY3NC0xZWJlMGQzYTVhM2IiLCJpYXQiOiIyMSBOb3YgMjMgMTEuMDMuNDciLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiam9obiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Ik1lbWJlciIsIlVzZXIgSUQiOiI4IiwiRGlzcGxheU5hbWUiOiJpYWRpaWtvIiwiRW1haWwiOiJ3b3Bkb2thb3BkQGZpb2FlLmRrIiwiQWdlIjoiMjgiLCJEb21haW4iOiJmaW9hZS5kayIsIlNlY3VyaXR5TGV2ZWwiOiIxIiwiZXhwIjoxNzAwNTY4MjI3LCJpc3MiOiJKV1RBdXRoZW50aWNhdGlvblNlcnZlciIsImF1ZCI6IkpXVFNlcnZpY2VCbGF6b3JXYXNtQ2xpZW50In0.fRLKfJFy7breCKrSlG9QMIQc8D02c93vtiUF7HZxZX7XyS06kC7pU30AsvHyUTc-ihxIEABW0K301YT-Z-HJJw";
+        string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKV1RTZXJ2aWNlQWNjZXNzVG9rZW4iLCJqdGkiOiJlNTU3NDA5My1mOTljLTQ1ODAtYmM1Yy1mZWQzMGY2NzM3YjciLCJpYXQiOiIyMyBOb3YgMjMgMDkuMDAuNDgiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiTUxHIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiTWVtYmVyIiwiVXNlciBJRCI6IjYiLCJEaXNwbGF5TmFtZSI6Ik1hcmMiLCJFbWFpbCI6ImhhaGFAeGQuY29tIiwiQWdlIjoiMjciLCJEb21haW4iOiJ4ZC5jb20iLCJTZWN1cml0eUxldmVsIjoiMSIsImV4cCI6MTcwMDczMzY0OCwiaXNzIjoiSldUQXV0aGVudGljYXRpb25TZXJ2ZXIiLCJhdWQiOiJKV1RTZXJ2aWNlQmxhem9yV2FzbUNsaWVudCJ9.GXASqoSQfmbjVaNvvgsGmXhO10jk_vjj3pb_ZZG9Qzmfo1jKACh9G6ZcfGbVsfUAJpC5dPn9FLH3MIQGzjn8Ug";
         Jwt = token;
         
         ClaimsPrincipal principal = CreateClaimsPrincipal();
 
         OnAuthStateChanged.Invoke(principal);
+    }
+
+    public UserDto GetUser()
+    {
+        return UserDto;
     }
 
     private static ClaimsPrincipal CreateClaimsPrincipal()
